3d432156d908927fa58b27f54d59e782aaeca213
Pashermgn <pavelmalikov@live.ru>
2023-02-22 21:21:53 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-2xl font-bold underline">
+        hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
